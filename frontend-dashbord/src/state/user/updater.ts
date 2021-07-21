ed9f628337a83747008b04e60c8139f001e9273a
elite0226 <elite0226@outlook.com>
2021-07-21 09:09:42 -0400

    default to dark mode

--- frontend-dashbord/src/state/user/updater.ts
+++ frontend-dashbord/src/state/user/updater.ts
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import { AppDispatch } from 'state';
-import { updateMediaDarkMode, updateUserDarkMode } from './actions';
-import { DARK_MODE_LOCALSTORAGE_KEY } from './reducer';
+import { AppDispatch } from "state";
+import { updateMediaDarkMode, updateUserDarkMode } from "./actions";
+import { DARK_MODE_LOCALSTORAGE_KEY } from "./reducer";
 
 export default function Updater(): null {
   const dispatch = useDispatch<AppDispatch>();
@@ -11,7 +11,9 @@ export default function Updater(): null {
   useEffect(() => {
     const userExistingDarkMode = Number(
       localStorage.getItem(DARK_MODE_LOCALSTORAGE_KEY)
-    );
+        ? localStorage.getItem(DARK_MODE_LOCALSTORAGE_KEY)
+        : 1
+    ); //  default to dark mode
 
     dispatch(
       updateUserDarkMode({ userDarkMode: Boolean(userExistingDarkMode) })
@@ -21,21 +23,21 @@ export default function Updater(): null {
       dispatch(updateMediaDarkMode({ mediaDarkMode: match.matches }));
     };
 
-    const match = window?.matchMedia('(prefers-color-scheme: dark)');
+    const match = window?.matchMedia("(prefers-color-scheme: dark)");
 
     dispatch(updateMediaDarkMode({ mediaDarkMode: match.matches }));
 
     if (match?.addListener) {
       match?.addListener(darkHandler);
     } else if (match?.addEventListener) {
-      match?.addEventListener('change', darkHandler);
+      match?.addEventListener("change", darkHandler);
     }
 
     return () => {
       if (match?.removeListener) {
         match?.removeListener(darkHandler);
       } else if (match?.removeEventListener) {
-        match?.removeEventListener('change', darkHandler);
+        match?.removeEventListener("change", darkHandler);
       }
     };
   }, [dispatch]);
