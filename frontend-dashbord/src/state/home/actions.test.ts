1bbf853068a21e70db1cbf47a9a6d5c4fa529dd8
elite0226 <elite0226@outlook.com>
2021-09-15 18:11:31 -0400

    home/actions.test.ts

---
+++ frontend-dashbord/src/state/home/actions.test.ts
@@ -0,0 +1,21 @@
+import { TotalStat, updateTotalStats } from './actions'
+
+describe('Home actions', () => {
+  describe('updateTotalStats', () => {
+    const mock: TotalStat = {
+      totalDailyTxCount: null,
+      totalDailyFeeVolumeUSD: null,
+      totalDailyVolumeUSD: null,
+      totalDepositsAllPoolsUSD: null,
+      totalLiquidityUtilization: null,
+      poolStats: null,
+    }
+
+    it('should create action with passed data', () => {
+      expect(updateTotalStats(mock)).toEqual({
+        type: 'home/updateTotalStats',
+        payload: mock
+      })
+    })
+  })
+})
