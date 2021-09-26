99ec903234d9f7cad21ba259a5155d66e1a6fa4c
elite0226 <elite0226@outlook.com>
2021-09-26 12:37:15 -0400

    components Dialog

---
+++ frontend-dashbord/src/components/Dialog/Dialog.test.tsx
@@ -0,0 +1,32 @@
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import * as Enzyme from "enzyme";
+import { shallow } from "enzyme";
+import { Dialog } from ".";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let wrapper: Enzyme.ShallowWrapper;
+const handleDialogClose = () => {
+  console.log("hello");
+};
+
+describe("Components / Dialog / Dialog", () => {
+  beforeEach(() => {
+    wrapper = shallow(
+      <Dialog
+        onClose={handleDialogClose}
+        aria-labelledby="simple-dialog-title"
+        open={true}
+      />
+    );
+  });
+
+  it("renders", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should be correct props", () => {
+    expect(wrapper.prop("open")).toEqual(true);
+    expect(wrapper.prop("aria-labelledby")).toEqual("simple-dialog-title");
+    expect(wrapper.prop("onClose")).toEqual(handleDialogClose);
+  });
+});
