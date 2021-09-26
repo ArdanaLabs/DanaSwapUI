9502015244eed4fc35e82374ac8b1facdcdedb31
elite0226 <elite0226@outlook.com>
2021-09-26 12:11:50 -0400

    component SwapButton

---
+++ frontend-dashbord/src/components/Button/SwapButton.test.tsx
@@ -0,0 +1,44 @@
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import * as Enzyme from "enzyme";
+import { shallow } from "enzyme";
+import { SwapButton } from ".";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock("react-redux", () => ({
+  useDispatch: () => {},
+  useSelector: () => ({
+    user: {
+      userDarkMode: null,
+      mediaDarkMode: false,
+      timestamp: "dfd",
+    },
+  }),
+}));
+
+let wrapper: Enzyme.ShallowWrapper;
+const onSwapButtonClick = () => {
+
+}
+
+describe("Components / Button / SwapButton", () => {
+  beforeEach(() => {
+    wrapper = shallow(
+      <SwapButton
+        style={{ margin: "0 40px" }}
+        onButtonClick={onSwapButtonClick}
+      />
+    );
+  });
+
+  it("renders", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should be correct props", () => {
+    expect(wrapper.prop("onClick")).toEqual(onSwapButtonClick);
+    expect(wrapper.prop("style")).toEqual({ margin: "0 40px" });
+  });
+  it("should contain 2 image tag", () => {
+    expect(wrapper.find("img")).toHaveLength(2);
+  });
+});
