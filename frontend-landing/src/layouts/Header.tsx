7da9c517ecca334a2fcadb17b58336a320e95af3
elite0226 <elite0226@outlook.com>
2021-10-07 09:45:02 -0400

    top-notch section

--- frontend-landing/src/layouts/Header.tsx
+++ frontend-landing/src/layouts/Header.tsx
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Box,
-  Link,
-  IconButton,
-  Drawer,
   useMediaQuery,
   Container,
 } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Hamburger from "hamburger-react";
 import cx from "classnames";
 
 import { useIsDarkMode } from "state/user/hooks";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import ThemeSwitch from "components/ThemeSwitch";
 import { Button } from "components/Button";
 
-import { navList } from "data";
 import LOGO_Blue from "assets/logo_blue.png";
 import LOGO_Text from "assets/logo_text.png";
 
@@ -95,17 +90,6 @@ const Header: React.FC = () => {
   const dark = useIsDarkMode();
   const classes = useStyles({ dark, mobile });
   const history = useHistory();
-  const { pathname } = useLocation<{ previous: string }>();
-
-  const [openMenu, setOpenMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setOpenMenu((prev) => !prev);
-  };
-
-  const isActiveURL = (link: string): boolean => {
-    return pathname.indexOf(link) > -1;
-  };
 
   const onConnectWallet = (event: any) => {
     console.log("connect wallet button clicked!");
@@ -119,46 +103,6 @@ const Header: React.FC = () => {
             <img src={LOGO_Blue} alt="Ardana Logo" />
             <img src={LOGO_Text} alt="Ardana Logo" />
           </Box>
-
-          {!mobile && (
-            <Box className={cx(classes.navBar)}>
-              {navList.map((navItem, index) => (
-                <Link
-                  className={isActiveURL(navItem.link) ? "active" : ""}
-                  key={index}
-                  href={navItem.link}
-                >
-                  {navItem.label}
-                </Link>
-              ))}
-            </Box>
-          )}
-
-          {mobile && (
-            <>
-              <IconButton
-                style={{ height: "48px", padding: 0 }}
-                onClick={() => setOpenMenu(!openMenu)}
-              >
-                <Hamburger
-                  size={24}
-                  distance={"lg"}
-                  color={theme.palette.text.primary}
-                  toggled={openMenu}
-                  toggle={setOpenMenu}
-                />
-              </IconButton>
-              <Drawer anchor={"top"} open={openMenu} onClose={toggleMenu}>
-                <Box className={cx(classes.navBar)}>
-                  {navList.map((navItem, index) => (
-                    <Link key={index} href={navItem.link}>
-                      {navItem.label}
-                    </Link>
-                  ))}
-                </Box>
-              </Drawer>
-            </>
-          )}
         </Box>
         <Box className={cx(classes.subHeader)}>
           <ThemeSwitch />
--- frontend-landing/src/pages/Landing/sections/top-notch.section.tsx
+++ frontend-landing/src/pages/Landing/sections/top-notch.section.tsx
@@ -6,46 +6,11 @@ import ScrollAnimation from "react-animate-on-scroll";
 import i18next from "i18next";
 
 import { useIsDarkMode } from "state/user/hooks";
-
-import LOGO_APPLE from "assets/img/landing/logos/logo_apple.svg";
-import LOGO_MICROSOFT from "assets/img/landing/logos/logo_microsoft.svg";
-import LOGO_BARCLAYS from "assets/img/landing/logos/logo_barclays.svg";
-import LOGO_MINA from "assets/img/landing/logos/logo_mina.svg";
-import LOGO_RAKUTEN from "assets/img/landing/logos/logo_rakuten.svg";
-import LOGO_IG from "assets/img/landing/logos/logo_ig.svg";
-import LOGO_CARDANO from "assets/img/landing/logos/logo_cardano.svg";
-import LOGO_EMURGO from "assets/img/landing/logos/logo_emurgo.svg";
-
-const TopNotchTeams = [
-  {
-    logo: LOGO_APPLE,
-  },
-  {
-    logo: LOGO_MICROSOFT,
-  },
-  {
-    logo: LOGO_BARCLAYS,
-  },
-  {
-    logo: LOGO_MINA,
-  },
-  {
-    logo: LOGO_RAKUTEN,
-  },
-  {
-    logo: LOGO_IG,
-  },
-  {
-    logo: LOGO_CARDANO,
-  },
-  {
-    logo: LOGO_EMURGO,
-  },
-];
+import { TopNotchTeams } from "data";
 
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
   bg: {
-    background: "#FFFFFF",
+    background: "#080E42",
     padding: "100px 0",
 
     [breakpoints.down("sm")]: {
@@ -57,11 +22,15 @@ const useStyles = makeStyles(({ palette, breakpoints }) => ({
     fontFamily: "Brandon Grotesque",
     fontStyle: "normal",
     fontWeight: 900,
-    fontSize: "64px",
+    fontSize: "80px",
     lineHeight: "100%",
     textAlign: "center",
-    color: "#202F9A",
-    padding: "20px",
+    color: "#F5FCFE",
+    whiteSpace: "pre-line",
+
+    "& > span": {
+      color: "#73D6F1",
+    },
 
     [breakpoints.down("sm")]: {
       fontSize: "48px",
@@ -72,11 +41,11 @@ const useStyles = makeStyles(({ palette, breakpoints }) => ({
     fontFamily: "Museo Sans",
     fontStyle: "normal",
     fontWeight: 300,
-    fontSize: "18px",
-    lineHeight: "150%",
+    fontSize: "25px",
+    lineHeight: "30px",
     textAlign: "center",
-    color: "#202020",
-    padding: "20px",
+    color: "#F5FCFE",
+    whiteSpace: "pre-line",
 
     [breakpoints.down("sm")]: {
       fontSize: "16px",
@@ -94,10 +63,17 @@ const TopNotchSection: React.FC = () => {
     <Box className={cx(classes.bg)}>
       <Container>
         <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
-          <Box className={cx(classes.title)}>
-            {i18next.t("PAGE.LANDING.TOP-NOTCH.TITLE")}
-          </Box>
+          <Box
+            className={cx(classes.title)}
+            dangerouslySetInnerHTML={{
+              __html: i18next.t("PAGE.LANDING.TOP-NOTCH.TITLE", {
+                interpolation: { escapeValue: false },
+              }),
+            }}
+          />
         </ScrollAnimation>
+
+        <Box mt="30px" />
 
         <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
           <Box className={cx(classes.content)}>
@@ -111,20 +87,15 @@ const TopNotchSection: React.FC = () => {
             flexWrap="wrap"
             alignItems="center"
             justifyContent="center"
-            p="20px"
+            mt="30px"
+            style={{ opacity: 0.8 }}
           >
             {TopNotchTeams.map((team, index) => (
-              <Box
-                key={index}
-                p={!mobile ? "0px 30px" : "10px"}
-                width={!mobile ? "auto" : "50%"}
-                textAlign="center"
-              >
+              <Box key={index} textAlign="center" px="10px">
                 <img
-                  src={team.logo}
-                  alt=""
-                  width="100%"
-                  height="100%"
+                  src={team}
+                  alt="team"
+                  height="45px"
                   style={{ maxWidth: "max-content" }}
                 />
               </Box>
@@ -136,4 +107,4 @@ const TopNotchSection: React.FC = () => {
   );
 };
 
-export default (TopNotchSection);
+export default TopNotchSection;
