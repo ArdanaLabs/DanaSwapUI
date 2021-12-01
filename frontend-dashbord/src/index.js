38f9edd47c77f2fde28a4035845e9709a51b012c
elite0226 <elite0226@outlook.com>
2021-12-01 15:49:48 -0500

    wallet integration

--- frontend-dashbord/src/index.js
+++ frontend-dashbord/src/index.js
@@ -6,19 +6,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// import Cardano from "services/cardano";
-import "services/inject";
-
 require("dotenv").config();
 
-// Cardano.init().then(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-// });
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
